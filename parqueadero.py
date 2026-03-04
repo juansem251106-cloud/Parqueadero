d2b16276387076101a1f62f0ee76f7830b42e6b2
Juan Sebastian Montaño Montilla <juansem251106@gmail.com>
2026-03-04 09:30:47 -0500

    cree un sistema que simula ser un parqueadero de autos

---
+++ parqueadero.py
@@ -0,0 +1,30 @@
+print(" Bienvenido seas al parqueadero ")
+
+espacio_total = 10
+carros_parqueados = 5
+espacio_disponible = 5
+
+print("1-Elegir espacio \n2-Retirar carro \n3-Salir del sistema")
+eleccion = input("seleccione una opcion: ")
+
+opcion_1 = "1"
+opcion_2 = "2"
+opcion_3 = "3" 
+
+if eleccion == "1":
+   if carros_parqueados < espacio_disponible:
+       carros_parqueados += 1
+       print("Carro parqueado")
+   else:
+       print("No hay espacio disponible por los momentos...")
+
+elif eleccion == "2":
+    if carros_parqueados > 0:
+        carros_parqueados -= 1
+        print("Carro retirado")
+        
+elif eleccion == 3:
+    print("Bye nigga")  
+
+else:
+    print("Pongase serio ciervo")
